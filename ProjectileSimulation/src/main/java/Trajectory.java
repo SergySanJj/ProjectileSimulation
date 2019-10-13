f92aa671b49290861c3c19a1f24af47a84afb6e3
Sergei Yarema <isara.isara8@gmail.com>
2019-10-14 01:06:08 +0200

    Add trajectory

--- ProjectileSimulation/src/main/java/Trajectory.java
+++ ProjectileSimulation/src/main/java/Trajectory.java
@@ -1,13 +1,20 @@
 import com.sun.javafx.geom.Vec2f;
 
+
 class Trajectory {
     public static Vec2f getCoords(DotParams params, float currentTime, float g) {
-
-        return new Vec2f(0.0f, 0.0f);
+        float y0 = params.getStartPos().y;
+        float x0 = params.getStartPos().x;
+        float alpha = (float) Math.toRadians(params.getStartAngle());
+        float vx = (float) (params.getStartSpeed() * Math.cos(alpha));
+        float vy = (float) (params.getStartSpeed() * Math.sin(alpha));
+        float x = x0 + currentTime * vx;
+        float y = y0 + currentTime * vy - g * currentTime * currentTime / 2.f;
+        return new Vec2f(x, y);
     }
 
     public static Vec2f getCoords(DotParams params, float currentTime) {
-        float g = 9.81f;
+        float g = 9.80665f;
         return getCoords(params, currentTime, g);
     }
 }
--- ProjectileSimulation/src/test/java/TrajectoryTest.java
+++ ProjectileSimulation/src/test/java/TrajectoryTest.java
@@ -5,9 +5,20 @@ import static org.junit.Assert.*;
 
 public class TrajectoryTest {
 
+    public static boolean assertEquals(Vec2f a, Vec2f b) {
+        float delta = Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
+        return (delta < 0.001f);
+    }
+
     @Test
     public void getCoords() {
-        DotParams dotParams = new DotParams(new Vec2f(0.f, 0.f), 45.f, 10.f);
+        DotParams dotParams = new DotParams(new Vec2f(0.f, 0.f), 45.f, 10f);
         assertEquals(new Vec2f(0.f, 0.f), Trajectory.getCoords(dotParams, 0.f));
+        assertEquals(new Vec2f(10.196f, -0.003f), Trajectory.getCoords(dotParams, 1.442f));
+
+        float g = 9.80665f;
+
+        assertEquals(new Vec2f(0.f, 0.f), Trajectory.getCoords(dotParams, 0.f, g));
+        assertEquals(new Vec2f(10.196f, -0.003f), Trajectory.getCoords(dotParams, 1.442f, g));
     }
 }
